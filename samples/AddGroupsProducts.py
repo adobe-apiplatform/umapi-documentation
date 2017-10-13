127aef2e1f2bba5ebeaad7a2f89cb01b0788e7a8
Philip Levy <plevy@adobe.com>
2017-10-12 21:31:35 -0700

    Production drop of sample code

---
+++ samples/AddGroupsProducts.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+
+#*************************************************************************
+#
+# ADOBE CONFIDENTIAL
+# ___________________
+#
+#  Copyright 2017 Adobe Systems Incorporated
+#  All Rights Reserved.
+#
+# NOTICE:  Adobe permits you to use, modify, and distribute this file in 
+# accordance with the terms of the Adobe license agreement accompanying it.
+# If you have received this file from a source other than Adobe, then your
+# use, modification, or distribution of it requires the prior written
+# permission of Adobe.
+#**************************************************************************
+
+import sys
+if sys.version_info[0] == 2:
+    from ConfigParser import RawConfigParser
+if sys.version_info[0] >= 3:
+    from configparser import RawConfigParser
+import json
+import requests
+
+
+
+# read configuration file
+config_file_name = "usermanagement.config"
+config = RawConfigParser()
+config.read(config_file_name)
+
+# server parameters
+host = config.get("server", "host")
+endpoint = config.get("server", "endpoint")
+
+# enterprise parameters
+domain = config.get("enterprise", "fed_domain")
+org_id = config.get("enterprise", "org_id")
+api_key = config.get("enterprise", "api_key")
+access_token = config.get("enterprise", "access_token")
+
+# method parameters
+url = "https://" + host + endpoint + "/action/" + org_id
+headers = {
+    "Content-type" : "application/json",
+    "Accept" : "application/json",
+    "x-api-key" : api_key,
+    "Authorization" : "Bearer " + access_token
+    }
+json_data = \
+[
+  {
+    "user" : "9fDxSrIn4bgfpa2@" + domain,
+    "do" : [
+      {
+        "add" : {
+          "group" : [
+            "Creative Cloud 1"
+          ]
+        }
+      }
+    ]
+  }
+]
+
+# prepare body
+body = json.dumps(json_data)
+
+# send http request
+res = requests.post(url, headers=headers, data=body)
+
+# print response
+print(res.status_code)
+print(res.headers)
+print(res.text)
+
+# parse response body
+if res.status_code == 200:
+  res_json_data = json.loads(res.text)
+  result = res_json_data["result"]
+  if result == "success":
+    print("Success");
+
+exit(res.status_code)
---
+++ samples/UserInformationByGroup.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python
+
+#*************************************************************************
+#
+# ADOBE CONFIDENTIAL
+# ___________________
+#
+#  Copyright 2017 Adobe Systems Incorporated
+#  All Rights Reserved.
+#
+# NOTICE:  Adobe permits you to use, modify, and distribute this file in 
+# accordance with the terms of the Adobe license agreement accompanying it.
+# If you have received this file from a source other than Adobe, then your
+# use, modification, or distribution of it requires the prior written
+# permission of Adobe.
+#**************************************************************************
+
+import sys
+if sys.version_info[0] == 2:
+    from ConfigParser import RawConfigParser
+if sys.version_info[0] >= 3:
+    from configparser import RawConfigParser
+import json
+import requests
+
+
+
+# read configuration file
+config_file_name = "usermanagement.config"
+config = RawConfigParser()
+config.read(config_file_name)
+
+# server parameters
+host = config.get("server", "host")
+endpoint = config.get("server", "endpoint")
+
+# enterprise parameters
+org_id = config.get("enterprise", "org_id")
+api_key = config.get("enterprise", "api_key")
+access_token = config.get("enterprise", "access_token")
+
+# method parameters
+page = 0
+group = "TestGroup1"
+url = "https://" + host + endpoint + "/users/" + org_id + "/" + str(page) + "/" + group
+headers = {
+    "Content-type" : "application/json",
+    "Accept" : "application/json",
+    "x-api-key" : api_key,
+    "Authorization" : "Bearer " + access_token
+    }
+
+# send http request
+res = requests.get(url, headers=headers)
+
+# print response
+print(res.status_code)
+print(res.headers)
+print(res.text.encode('utf-8'))
+
+# parse response body
+if res.status_code == 200:
+    res_json_data = json.loads(res.text)
+    result = res_json_data["result"]
+    if result == "success":
+        lastPage = res_json_data["lastPage"]
+        users = res_json_data["users"]
+        print("Total Users returned " + str(len(users)) + ", last page? " + str(lastPage))
+        
+exit(res.status_code)
