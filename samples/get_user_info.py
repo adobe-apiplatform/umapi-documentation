832bc47211984b76918750574e6805681b8271c8
Lucian N <lneagu@adobe.com>
2023-05-17 14:35:15 +0300

    Update get_user_info.py

--- samples/get_user_info.py
+++ samples/get_user_info.py
@@ -16,6 +16,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
+# this script shows how to retrive account information for a given Admin Console account
 # https://adobe-apiplatform.github.io/umapi-documentation/en/api/getUser.html
 # GET /v2/usermanagement/organizations/{orgId}/users/{userString}
 
