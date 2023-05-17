fd24b2c4a221ec9926898a98d07215736fc836bc
Lucian N <lneagu@adobe.com>
2023-05-17 14:32:17 +0300

    Update get_groups_and_profiles.py

--- samples/get_groups_and_profiles.py
+++ samples/get_groups_and_profiles.py
@@ -16,6 +16,7 @@
 # OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
 
+# This sample script shows how to get all groups and profiles associated with the Org
 # https://adobe-apiplatform.github.io/umapi-documentation/en/api/group.html
 # GET /v2/usermanagement/groups/{orgId}/{page}
 
